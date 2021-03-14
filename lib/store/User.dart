b92add17444e1cb0ee9b5301458e8d44d0da7799
vcqzero <1250857765@qq.com>
2021-03-14 17:45:21 +0800

    添加routeObserver

--- lib/store/User.dart
+++ lib/store/User.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'package:get_storage/get_storage.dart';
 
-final _storage = GetStorage();
+GetStorage _getBox() => GetStorage('userStorage');
 
 class User {
   /// 定义之后不可修改，
@@ -36,7 +37,7 @@ class User {
   static Future<void> store(Map? userMap) async {
     String userJson = jsonEncode(userMap);
     try {
-      await _storage.write(storageKey, userJson);
+      await _getBox().write(storageKey, userJson);
       print('保存user success');
     } catch (e) {
       print('保存user 数据出错');
@@ -48,16 +49,17 @@ class User {
   User.build() {
     Map map = {};
     try {
-      map = jsonDecode(_storage.read(storageKey)) ?? {};
+      String? mapJson = _getBox().read(storageKey);
+      if (mapJson != null) map = jsonDecode(mapJson);
+
+      _setBasicInfo(map);
+      _setRole(map);
+      _setWorkyard(map);
+      _setCompany(map);
+      _setJpush(map);
     } catch (e) {
-      print('解析user\'s map 数据出错');
-      print(e);
+      log('BUILD USER ERROR', error: e);
     }
-    _setBasicInfo(map);
-    _setRole(map);
-    _setWorkyard(map);
-    _setCompany(map);
-    _setJpush(map);
   }
 
   _setBasicInfo(Map map) {
