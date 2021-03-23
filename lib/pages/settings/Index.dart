6836b3657f15325beaadf27e253d06ae43372349
vcqzero <1250857765@qq.com>
2021-03-23 23:04:31 +0800

    添加image_picker demo

--- lib/pages/settings/Index.dart
+++ lib/pages/settings/Index.dart
@@ -1,9 +1,12 @@
+import 'dart:developer';
+
 import 'package:app/app/Config.dart';
 import 'package:app/pages/about/Index.dart';
 import 'package:app/store/User.dart';
 import 'package:app/widgets/MyAppBar.dart';
 import 'package:app/widgets/MyTile.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SettingsPage extends StatefulWidget {
   static const routeName = "SettingsPage";
@@ -15,10 +18,17 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   late User _user;
+  final picker = ImagePicker();
   @override
   void initState() {
     super.initState();
     _user = User.cache();
+  }
+
+  _handleEditAvatar() async {
+    log('修改头像');
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+    if (pickedFile != null) print(pickedFile.path);
   }
 
   @override
@@ -48,6 +58,7 @@ class _SettingsPageState extends State<SettingsPage> {
                 backgroundImage:
                     NetworkImage(_user.avatar ?? Config.defaultAvatar),
               ),
+              onTap: _handleEditAvatar,
             ),
             Divider(
               height: 1,
