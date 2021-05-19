2028cbb5db0a4a3bae23a0f67d730b6048ca8dca
qinchong <qinchong1@xiaomi.com>
2021-05-19 09:55:31 +0800

    添加地图move交互

--- lib/pages/common/TheAmapPage.dart
+++ lib/pages/common/TheAmapPage.dart
@@ -1,6 +1,4 @@
-import 'package:app/utils/MyLocation.dart';
 import 'package:app/widgets/MyAppBar.dart';
-import 'package:app/widgets/MyButton.dart';
 import 'package:flutter/material.dart';
 import 'package:amap_flutter_map/amap_flutter_map.dart';
 import 'package:amap_flutter_base/amap_flutter_base.dart';
@@ -14,31 +12,32 @@ class TheAmapPage extends StatefulWidget {
 }
 
 class _TheAmapPageState extends State<TheAmapPage> {
-  MyLocation? _myLocation;
-  onLocated(LocationResult result) {
-    print('获取到定位数据');
-    print(result.address);
-    print(result.latitude);
-    print(result.longitude);
-  }
+  // MyLocation? _myLocation;
+  AMapController? _mapController;
+  String? _mapContentApprovalNumber; // 普通地图审图号
+  String? _satelliteImageApprovalNumber; // 卫星地图审图号
+
+  // onLocated(LocationResult result) {
+  //   print('获取到定位数据');
+  //   print(result.address);
+  //   print(result.latitude);
+  //   print(result.longitude);
+  // }
 
   @override
   void initState() {
-    _myLocation = MyLocation(onLocated: onLocated);
+    // _myLocation = MyLocation(onLocated: onLocated);
     super.initState();
     _checkPermissions();
   }
 
-  static final CameraPosition _kInitialPosition = const CameraPosition(
-    target: LatLng(39.909187, 116.397451),
-    zoom: 10.0,
-  );
-  final List<Permission> needPermissionList = [
-    Permission.location,
-    Permission.storage,
-    Permission.phone,
-  ];
+  /// 检查地图所需权限
   void _checkPermissions() async {
+    final List<Permission> needPermissionList = [
+      Permission.location,
+      Permission.storage,
+      Permission.phone,
+    ];
     Map<Permission, PermissionStatus> statuses =
         await needPermissionList.request();
     statuses.forEach((key, value) {
@@ -52,26 +51,108 @@ class _TheAmapPageState extends State<TheAmapPage> {
     _checkPermissions();
   }
 
-  AMapController? _mapController;
-  void onMapCreated(AMapController controller) {
+  void _onMapCreated(AMapController controller) {
+    _mapController = controller;
     setState(() {
-      _mapController = controller;
-      // getApprovalNumber();
+      getApprovalNumber();
     });
+  }
+
+  /// 获取地图审图号
+  void getApprovalNumber() async {
+    // 普通地图审图号
+    _mapContentApprovalNumber =
+        await _mapController?.getMapContentApprovalNumber();
+    // 卫星地图审图号
+    _satelliteImageApprovalNumber =
+        await _mapController?.getSatelliteImageApprovalNumber();
+  }
+
+  void _onCameraMoveEnd(CameraPosition cameraPosition) {
+    if (null == cameraPosition) {
+      return;
+    }
+    print('_onCameraMoveEnd===> ${cameraPosition.toMap()}');
   }
 
   @override
   Widget build(BuildContext context) {
+    // 创建地图
     final AMapWidget map = AMapWidget(
-      initialCameraPosition: _kInitialPosition,
-      onMapCreated: onMapCreated,
+      // 设置地图center坐标
+      initialCameraPosition: const CameraPosition(
+        target: LatLng(39.909187, 116.397451),
+        zoom: 15.0,
+      ),
+      onMapCreated: _onMapCreated,
+      onCameraMoveEnd: _onCameraMoveEnd,
     );
+
     return Scaffold(
       appBar: MyAppBar.build(title: 'Amap 地图'),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: map,
+      body: Column(
+        children: [
+          Container(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height / 2,
+                  child: map,
+                ),
+                Positioned(
+                  right: 2,
+                  bottom: 2,
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          _mapContentApprovalNumber ?? '',
+                          style: TextStyle(
+                            fontSize: 10,
+                          ),
+                        ),
+                        Text(
+                          _satelliteImageApprovalNumber ?? '',
+                          style: TextStyle(
+                            fontSize: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                Text('data'),
+                Text('data'),
+                Text('data'),
+                Text('data'),
+                Text('data'),
+                Text('data'),
+                Text('data'),
+                Text('data'),
+                Text('data'),
+                Text('data'),
+                Text('data'),
+                Text('data'),
+                Text('data'),
+                Text('data'),
+                Text('data'),
+                Text('data'),
+                Text('data'),
+                Text('data'),
+                Text('data'),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
